ee53f827bb096c12e133dcfee0af487504f36a16
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2025-11-14 10:47:28 -0300

    Variáveis em JS

---
+++ js/app.js
@@ -0,0 +1,41 @@
+
+// Variáveis em JS
+
+/*
+// VAR
+var valorA = 5;
+console.log(valorA);
+
+if (1 == 1) {
+    var valorA = "OK";
+    console.log(valorA);
+}
+
+console.log(valorA);
+// O "tipo" var tem o defeito de permitir que
+// seja criada uma variável com o mesmo nome
+// de uma variável qe já existe, sobrescrever
+// a anterior.
+*/
+
+/*
+// LET
+let valorB = 5;
+console.log(valorB);
+
+if (1 == 1) {
+    let valorB = "OK";
+    console.log(valorB);
+}
+
+console.log(valorB);
+*/
+
+// CONST
+let contador;
+
+const valorC = "valorC";
+console.log(valorC);
+
+valorC = "Marnei";
+console.log(valorC);

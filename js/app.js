06c1fcd4db768259460b51875f9e8e8fd3d30c61
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2025-11-14 11:57:57 -0300

    Variáveis em JS

--- js/app.js
+++ js/app.js
@@ -35,7 +35,52 @@ console.log(valorB);
 let contador;
 
 const valorC = "valorC";
-console.log(valorC);
+// console.log(valorC);
 
 // valorC = "Marnei";
 // console.log(valorC);
+
+// Quando devo usar LET e quando devo usar CONST?
+
+// Exercício 7 (da lista): Escreva um algoritmo que
+// receba 3 notas de um estudante. Calcule a média
+// das notas e mostre na tela.
+
+// 1. Declaração da Variáveis
+let nota1;
+let nota2;
+let nota3;
+
+// Mostra o tipo
+console.log(typeof(nota1));
+
+// 2. Atribuição de Valores (ENTRADA)
+nota1 = 7;
+nota2 = 6.3;
+nota3 = 3;
+
+// 3. Cálculos e Validações (PROCESSAMENTO)
+const media = (nota1 + nota2 + nota3) / 3;
+
+// 4 Mostrar o Resultado (SAÍDA)
+console.log(media.toFixed(1));
+// console.log(media);
+
+/* TIPOS de VARIÁVEIS
+    Inteiro     int     5  9  23
+    Decimal     float   1.25    0.75
+    Caractere   char    "m" "A" "@"
+    Texto       string  "Eu sou o Marnei"
+    Booleano    bool    true ou false
+*/
+
+const nome = "Marnei";
+const nivel = "ótimo";
+
+// Concatenação
+// const teste = "Eu sou o " + nome + ", sou um " + nivel + " professor";
+
+// Template Literal (Template String)
+const teste = `Eu sou o ${nome}, sou um "${nivel}" professor`;
+
+console.log(teste);
